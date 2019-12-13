114389281de7a6378a8de2292510b4004ae27a15
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-12-13 12:14:41 +0000

    Added template for detail in questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -14,11 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)  # raises 404 if object does not exist
+    return render(request, 'polls/details.html', {'question': question})
 
 
 def results(request, question_id):
